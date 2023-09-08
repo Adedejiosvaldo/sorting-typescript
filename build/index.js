2c7f7bf3e07f76bec624e35b3f448c346464e516
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-09-08 11:05:33 +0100

    Feature - Implemented sorting logic

--- build/index.js
+++ build/index.js
@@ -2,12 +2,24 @@
 // Bubble Sort
 //Bubble Scaffolfing
 class Sorter {
-    //   collection: number[];
+    //collection: number[];
     constructor(collection) {
         this.collection = collection;
     }
-    //SOrt function
-    sort() { }
+    //Sorting function
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftSide = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftSide;
+                }
+            }
+        }
+    }
 }
-const sorter = new Sorter([1, 2, 3]);
-console.log(sorter);
+const sorter = new Sorter([1, -5, 3]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 // Bubble Sort
-
 //Bubble Scaffolfing
-
 class Sorter {
-  //   collection: number[];
+  //collection: number[];
   constructor(public collection: number[]) {}
 
-  //SOrt function
-  sort(): void {}
+  //Sorting function
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftSide = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftSide;
+        }
+      }
+    }
+  }
 }
 
-const sorter = new Sorter([1, 2, 3]);
-console.log(sorter);
+const sorter = new Sorter([1, -5, 3]);
+sorter.sort();
+console.log(sorter.collection);
