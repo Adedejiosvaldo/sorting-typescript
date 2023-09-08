4c9d2be5dde2f0adb13a6ca326c09296c03b112f
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-09-08 10:49:14 +0100

    Feature - Sorter scaffold

--- build/index.js
+++ build/index.js
@@ -1,2 +1,13 @@
 "use strict";
-const hell = "Hello";
+// Bubble Sort
+//Bubble Scaffolfing
+class Sorter {
+    //   collection: number[];
+    constructor(collection) {
+        this.collection = collection;
+    }
+    //SOrt function
+    sort() { }
+}
+const sorter = new Sorter([1, 2, 3]);
+console.log(sorter);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-const hell: string = "Hello";
+// Bubble Sort
+
+//Bubble Scaffolfing
+
+class Sorter {
+  //   collection: number[];
+  constructor(public collection: number[]) {}
+
+  //SOrt function
+  sort(): void {}
+}
+
+const sorter = new Sorter([1, 2, 3]);
+console.log(sorter);
